5672cb0d27a2c676dd0f726643ef230984a88c94
Olivia Macedo <liom62313@gmail.com>
2018-08-18 23:09:26 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,7 @@
 def display_rainbow(myarray)
-
+  array1 = myarray
+  comparray = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+  if(array1[0] == comparray[0])
+    puts "R"
+  end
 end# Write your #display_rainbow method here
