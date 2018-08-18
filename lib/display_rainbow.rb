d395e73489ce268436bc98c76fd96b71490ccb5c
Olivia Macedo <liom62313@gmail.com>
2018-08-18 23:17:05 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,24 +2,18 @@ def display_rainbow(myarray) # Write your #display_rainbow method here
   array1 = myarray
   comparray = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
   if(array1[0] == comparray[0])
-    print "R: red"
-  end
-  if(array1[1] == comparray[1])
-    print ", O: orange"
-  end
-  if(array1[2] == comparray[2])
-    print ", Y: yellow"
-  end
-  if(array1[3] == comparray[3])
-    print ", G: green"
-  end
-  if(array1[4] == comparray[4])
-    print ", B: blue"
-  end
-  if(array1[5] == comparray[5])
-    print ", I: indigo"
-  end
-  if(array1[6] == comparray[6])
-    print ", V: violet"
+    if(array1[1] == comparray[1])
+      if(array1[2] == comparray[2])
+        if(array1[3] == comparray[3])
+          if(array1[4] == comparray[4])
+            if(array1[5] == comparray[5])
+              if(array1[6] == comparray[6])
+                puts "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
+              end
+            end
+          end
+        end
+      end
+    end
   end
 end
